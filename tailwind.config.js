5afaa63125e767e373f7a20b6f23d024189bde40
devruhul <ruhulamin262001@gmail.com>
2022-08-21 21:14:56 +0600

    feat: add xs size to responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': { 'min': '320px', 'max': '425px' },
+      ...defaultTheme.screens,
+    },
     extend: {},
     colors: {
       "purple-text": "#DF453F",

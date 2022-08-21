f025f9416960302ddf8edfa09dba13c1aedec73a
devruhul <ruhulamin262001@gmail.com>
2022-08-21 21:17:29 +0600

    add: added navbar and home nav, login and register page init

--- src/components/HomeNav.js
+++ src/components/HomeNav.js
@@ -1,11 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomeNav = () => {
-    return (
-        <div>
-          {/* <h2>Hello</h2> */}
-        </div>
-    );
+  return (
+    <header className="footer flex justify-between content-center p-4 bg-neutral text-neutral-content">
+      <div className=" grid-flow-col flex flex-center">
+        <i className="fa-solid fa-phone mt-1"></i>
+        <a href="tel:+8801764896633"><span>01764896633</span></a>
+      </div>
+      <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <i className="fa-solid fa-key mt-1"></i>
+        <Link to="login">Login</Link>
+        <Link to="register">Register</Link>
+        <a href='https://www.facebook.com/devruhulaminmahfuj' target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook"></i>
+        </a>
+        <a href="https://twitter.com/dev_ruhul" target="_blank" rel="noreferrer"><i className="fa-brands fa-twitter"></i></a>
+        <a href="https://www.linkedin.com/in/devruhul/" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a>
+      </div>
+    </header>
+  );
 };
 
 export default HomeNav;
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -4,33 +4,33 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div class="navbar bg-base-300 purple-text">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="navbar bg-base-300 purple-text">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <i className="fa-solid fa-bars-staggered"></i>
                     </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul className="menu menu-compact dropdown-content mt-3 p-2 shadow  rounded-box w-100">
                         <Link to="/"><li><a>Home</a></li></Link>
-                        <Link to="/bicycles"><li><a>Bicycles</a></li></Link>
+                        <Link to="bicycles"><li><a>Bicycles</a></li></Link>
                         {/* <Link to="/"><li><a>Dashboard</a></li></Link> */}
-                        <Link to="/reviews"><li><a>Reviews</a></li></Link>
-                        <Link to="/contact"><li><a>Contact</a></li></Link>
+                        <Link to="reviews"><li><a>Reviews</a></li></Link>
+                        <Link to="contact"><li><a>Contact</a></li></Link>
                     </ul>
                 </div>
-                <Link to="/" ><a class="btn btn-ghost normal-case text-xl">Yokoo</a></Link>
+                <Link to="/" ><a className="btn btn-ghost normal-case text-xl">Yokoo</a></Link>
             </div>
-            <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal p-0">
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
                     <Link to="/"><li><a>Home</a></li></Link>
-                    <Link to="/bicycles"><li><a>Bicycles</a></li></Link>
+                    <Link to="bicycles"><li><a>Bicycles</a></li></Link>
                     {/* <Link to="/"><li><a>Dashboard</a></li></Link> */}
-                    <Link to="/reviews"><li><a>Reviews</a></li></Link>
-                    <Link to="/contact"><li><a>Contact</a></li></Link>
+                    <Link to="reviews"><li><a>Reviews</a></li></Link>
+                    <Link to="contact"><li><a>Contact</a></li></Link>
                 </ul>
             </div>
-            <div class="navbar-end">
-                <a class="btn nav-text">Login</a>
+            <div className="navbar-end">
+                <Link to="login"><a className="btn nav-text">Logout</a></Link>
             </div>
         </div>
     );

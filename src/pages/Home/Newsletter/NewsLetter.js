df83785300da7b20548bdfefa46dcbf5041655d7
devruhul <ruhulamin262001@gmail.com>
2022-08-22 15:38:49 +0600

    fix: change some ityle in input field

--- src/pages/Home/Newsletter/NewsLetter.js
+++ src/pages/Home/Newsletter/NewsLetter.js
@@ -21,7 +21,7 @@ const NewsLetter = () => {
                                 <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                     <i class="fa-solid fa-envelope"></i>
                                 </div>
-                                <input class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter your email" type="email" id="email" required="" />
+                                <input class="block p-3 pl-10 w-full text-md text-purple-color bg-gray-50 rounded-lg border-2 border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-700 focus:border-primary-700" placeholder="Enter your email" type="email" id="email" required="" />
                             </div>
                             <div>
                                 <button type="submit" class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 btn btn-primary">Subscribe</button>

e89193679fc1a390fa51f225f962a666b7407a68
devruhul <ruhulamin262001@gmail.com>
2022-08-22 17:26:44 +0600

    feat: added footer page with responsive also added custome bg color

--- src/components/HomeNav.js
+++ src/components/HomeNav.js
@@ -13,10 +13,6 @@ const HomeNav = () => {
         <i className="fa-solid fa-key mt-1"></i>
         <Link to="login">Login</Link>
         <Link to="register">Register</Link>
-        <a href='https://www.facebook.com/devruhulaminmahfuj' target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook"></i>
-        </a>
-        <a href="https://twitter.com/dev_ruhul" target="_blank" rel="noreferrer"><i className="fa-brands fa-twitter"></i></a>
-        <a href="https://www.linkedin.com/in/devruhul/" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a>
       </div>
     </header>
   );
--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -11,8 +11,8 @@ const Banner = () => {
                 <div class="hero-overlay bg-opacity-60"></div>
                 <div class="hero-content text-center text-neutral-content">
                     <div class="max-w-md">
-                        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p class="mb-5">We’re a bike shop, a family and a team of riders, helping folks in New York explore their passion for cycling. Biking is more than transportation or exercise, it’s a lifestyle. Whether you need help with where to ride or are in need.</p>
+                        <h1 class="mb-5 text-5xl font-bold"> <span className='text-purple-color opacity-90'>Hello</span> there</h1>
+                        <p class="mb-5 text-2xl">We’re a bike shop, a family and a team of riders, helping folks in New York explore their passion for cycling. Biking is more than transportation or exercise, it’s a lifestyle. Whether you need help with where to ride or are in need.</p>
                         <Link to="/bicycles" class="btn btn-primary">Explore</Link>
                     </div>
                 </div>

dbf79dc845e3b9167f47380315ab576306e1f2a5
devruhul <ruhulamin262001@gmail.com>
2022-08-26 01:40:09 +0600

    feat: added my orders page to show current user bookings and order status and in banner added current user name

--- src/Dashboard/MyOrders/MyOrders.js
+++ src/Dashboard/MyOrders/MyOrders.js
@@ -1,9 +1,49 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
+import useFirebase from '../../Hooks/useFirebase';
 
 const MyOrders = () => {
+    const { yokooUser } = useFirebase()
+    // load all bookings using react query nad current user email
+    const {  isError, error, status,  data } = useQuery(
+        ['myOrders', yokooUser.email], async () => await axios.get(`http://localhost:5000/booking/${yokooUser.email}`,
+            {
+                // The query will not execute until the userId exists
+                enabled: !!yokooUser.email,
+            }
+        ))
+
+    if (status === 'loading') {
+        return <div>Loading...</div>;
+    }
+    // error message query
+    if (isError) {
+        return <div>Error: {error.message}</div>;
+    }
     return (
         <div>
-            <h2>My Orders</h2>
+              <div className="m-6 text-3xl font-bold text-center text-gray-800 dark:text-white">
+                    My <span className='text-purple-color'>Orders </span>
+                </div>
+            <div className="grid grid-cols-3 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8">
+                {
+                    data?.data.map(order => (
+                        <div key={order._id} className="card w-50 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <img src={order?.bicycleImg} alt="Shoes" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title hover:text-purple-color cursor-pointer">{order?.bicycleTitle}</h2>
+                                <p>{order?.bicycleDescription}</p>
+                                <p>${order?.bicyclePrice}</p>
+                                <h2 className="text-base-300 p-4 rounded-md bg-purple-color"> {order.orderStatus}</h2>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
         </div>
     );
 };
--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import slider1 from '../../../assets/images/slide-1.png';
+import useFirebase from '../../../Hooks/useFirebase';
 
 const Banner = () => {
+    const { yokooUser } = useFirebase()
     return (
         <div>
             <div className="hero min-h-screen" style={{
@@ -11,7 +13,7 @@ const Banner = () => {
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold"> <span className='text-purple-color opacity-90'>Hello</span> there</h1>
+                        <h1 className="mb-5 text-5xl font-bold"> Hello<span className='text-purple-color opacity-90'> {yokooUser?.displayName}</span> </h1>
                         <p className="mb-5 text-2xl">We’re a bike shop, a family and a team of riders, helping folks in New York explore their passion for cycling. Biking is more than transportation or exercise, it’s a lifestyle. Whether you need help with where to ride or are in need.</p>
                         <Link to="/bicycles" className="btn btn-primary">Explore</Link>
                     </div>

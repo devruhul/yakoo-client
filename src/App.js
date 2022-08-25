3d070284d6e3459b332b76ec5ffa7716b0704c65
devruhul <ruhulamin262001@gmail.com>
2022-08-25 11:26:12 +0600

    feat: use react-query to get data and show the bicycle data

--- src/App.js
+++ src/App.js
@@ -17,30 +17,39 @@ import NotFound from './pages/Home/NotFound/NotFound';
 import Reviews from './pages/Home/Reviews/Reviews';
 import Footer from './pages/Shared/Footer/Footer';
 import Header from './pages/Shared/Header/Header';
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 
 function App() {
+  const queryClient = new QueryClient()
+
   return (
     <div className="App">
-      <HomeNav />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="bicycles" element={
-          <PrivateRoute>
-            <Bicycles />
-          </PrivateRoute>
-        } />
-        <Route path="dashboard" element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }>
-          <Route index element={<AddBicycle />} />
-          <Route path="/dashboard/payment" element={<Payment />} />
-          <Route path="/dashboard/addBicycle" element={<AddBicycle />} />
-          <Route path="/dashboard/myOrders" element={<MyOrders />} />
-          <Route path="/dashboard/bicycleReview" element={<BicycleReview />} />
-          {/* <Route path="/dashboard/ordersList" element={
+      <QueryClientProvider client={queryClient}>
+        <HomeNav />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="bicycles" element={
+            <PrivateRoute>
+              <Bicycles />
+            </PrivateRoute>
+          } />
+          <Route path="dashboard" element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }>
+            <Route index element={<AddBicycle />} />
+            <Route path="/dashboard/payment" element={<Payment />} />
+            <Route path="/dashboard/addBicycle" element={<AddBicycle />} />
+            <Route path="/dashboard/myOrders" element={<MyOrders />} />
+            <Route path="/dashboard/bicycleReview" element={<BicycleReview />} />
+            {/* <Route path="/dashboard/ordersList" element={
             <OrdersLists />
           } />
           <Route path="/dashboard/manageServices" element={
@@ -50,15 +59,19 @@ function App() {
             <MakeAdmin />
           } /> */}
 
-        </Route>
-        <Route path="explore" element={<Explore />} />
-        <Route path="reviews" element={<Reviews />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-      <Footer />
+          </Route>
+          <Route path="explore" element={<Explore />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+        <ReactQueryDevtools initialIsOpen={true} />
+
+      </QueryClientProvider>
+
     </div>
   );
 }
--- src/pages/Home/Bicycles/Bicycles.js
+++ src/pages/Home/Bicycles/Bicycles.js
@@ -1,59 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import getBicycles from '../../../components/getBicycles'
+import { useQuery } from '@tanstack/react-query';
 
 const Bicycles = () => {
+
+    const { data, isLoading, isError } = useQuery(["bicycles"], getBicycles)
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError) {
+        return <div>Error! + `${isError}`</div>;
+    }
+
     return (
         <div className='mt-12 container'>
             <h3 className='text-4xl font-bold my-1  opacity-90'>Discover <br /> our new<span className="text-purple-color opacity 90"> arrivals</span></h3>
             <div className="grid grid-cols-4 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-                <div className="card w-50 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
+
+                {
+                    data?.map(bicycle => (
+                        <div key={bicycle._id} className="card w-50 bg-base-100 shadow-xl  hover:bg-footer-bg hover:scale-90 transition ease-in-out delay-150 bg-blue-500 hover:translate-y-1 hover:bg-indigo-500 duration-300">
+                            <figure className="px-10 pt-10">
+                                <img src={bicycle?.imageLink} alt="Shoes" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title hover:text-purple-color cursor-pointer">{bicycle?.bicycleName}</h2>
+                                <p>{bicycle?.description}</p>
+                                <p>${bicycle?.price}</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="card w-50 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-50 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-50 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
+                    ))
+                }
             </div>
             <Link to="explore" ><button className="btn btn-primary my-20">Explore More Bicycles</button></Link>
         </div>

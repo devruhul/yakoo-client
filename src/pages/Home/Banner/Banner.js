94f9bfc1c1bf0c18b46bce5cfdd01bd12e094624
devruhul <ruhulamin262001@gmail.com>
2022-08-27 01:07:59 +0600

    fix styles: change some styles

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -13,7 +13,7 @@ const Banner = () => {
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold"> Hello<span className='text-purple-color opacity-90'> {yokooUser?.displayName}</span> </h1>
+                        <h1 className="mb-5 text-4xl font-bold"> Hello<span className='text-purple-color opacity-90'> {yokooUser?.displayName}</span> </h1>
                         <p className="mb-5 text-2xl">We’re a bike shop, a family and a team of riders, helping folks in New York explore their passion for cycling. Biking is more than transportation or exercise, it’s a lifestyle. Whether you need help with where to ride or are in need.</p>
                         <Link to="/bicycles" className="btn btn-primary">Explore</Link>
                     </div>
